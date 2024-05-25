1ae8c3859667be394a7f1a5b75c33d213e499f64
gabriel0138 <hackerdeus111@gmail.com>
2024-05-25 09:19:28 +0900

    Project's Initial Commit

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,32 +1,32 @@
-// This is a JavaScript file
+$(document).ready(function() {
+    $(document).on("click", "#alerta", function(){
+        alert("minha Mensagem");
+    });
 
-$(document).on("click", "#alerta", function(){
-    function retorno() {
-        // Função de retorno vazia
-    }
-    navigator.notification.alert("minha Mensagem", retorno, "Aviso!","Aceito");
-});
-
-$(document).on("click", "#confirm", function(){
-    function confirma(buttonIndex) {
-        if(buttonIndex == 1) {
-            navigator.notification.alert("Escolheu a opção A");
-        } else {
-            navigator.notification.alert("Escolheu a opção B");
+    $(document).on("click", "#confirm", function(){
+        function confirma(buttonIndex) {
+            if(buttonIndex == 1) {
+                alert("Escolheu a opção A");
+            } else {
+                alert("Escolheu a opção B");
+            }
         }
-    }
-    navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:", ['A','B']);
-});
+        var resposta = confirm("Escolha A ou B");
+        if (resposta) {
+            confirma(1);
+        } else {
+            confirma(2);
+        }
+    });
 
-$(document).on("click", "#beep", function(){
-    navigator.notification.beep(4);
-});
-document.addEventListener("deviceready", onDeviceReady, false);
+    $(document).on("click", "#beep", function(){
+        // Não há suporte direto para beep em JavaScript padrão
+        console.log("Beep não é suportado em JavaScript padrão.");
+    });
 
-function onDeviceReady() {
-    if (navigator.vibrate) {
+    if ("vibrate" in navigator) {
         console.log("Vibração é suportada");
     } else {
         console.log("Vibração não é suportada");
     }
-}
+});
